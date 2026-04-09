ea9006a35cebc66551e67d3a3ff2008e50e12e75
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-04-09 00:12:23 -0400

    Task 2: add floor function annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Define a typed floor helper."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a floating-point number."""
+    return math.floor(n)
