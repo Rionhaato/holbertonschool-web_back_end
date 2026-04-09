18b27f1a048156bdd1df6a03b6aa92c1ec3135a9
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-04-09 00:12:33 -0400

    Task 3: add float to string annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define a typed float-to-string helper."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floating-point number."""
+    return str(n)
