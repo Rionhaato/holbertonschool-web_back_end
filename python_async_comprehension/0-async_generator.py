cad1b7e04f4b97996e5b80d4a75d4f6ea7525b11
Joshua Santiago <joshua.d.santiago92@gmail.com>
2026-04-10 23:57:25 -0400

    Fix async generator return annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield ten random floats between 0 and 10 with one-second pauses."""
     for _ in range(10):
         await asyncio.sleep(1)
