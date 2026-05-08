4e6d02d404d339eceda484f2543b677afccc07ce
Rionhaato <joshua.d.santiago92@gmail.com>
2026-05-07 20:19:28 -0400

    Add async upload user handler

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+
+    return {
+      photo,
+      user,
+    };
+  } catch {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
